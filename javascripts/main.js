50590d9f4867d3d7cda5374eaf94e95e82d7649e
compuzip <mazzid@gmail.com>
2013-08-09 19:55:18 -0400

    Bootstrap the example app

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,24 @@
-console.log('This would be the main JS file.');
+'use strict';
+
+// Bootstrap function
+angular.element(document).ready(function() {
+    angular.bootstrap(angular.element('#main'), [ 'circuitBrowser' ]);
+});
+
+// Route Provider
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/',
+        {
+            templateUrl: '/templates/main.html',
+            controller: 'CircuitCtrl'
+        })
+        .when('/cluster/:cluster',
+        {
+            templateUrl: '/templates/main.html',
+            controller: 'CircuitCtrl'
+        })
+        .otherwise({
+            template: "<h1>I'm sorry Dave, I'm afraid I can't do that</h1>"
+        })
+});
